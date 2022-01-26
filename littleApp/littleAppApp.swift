34236c8e9fba2cc9419d575ba7119aad18a75cd6
Jun <et244157@gmail.com>
2022-01-26 22:53:53 +0800

    Initial Commit

---
+++ littleApp/littleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  littleAppApp.swift
+//  littleApp
+//
+//  Created by Jun3631 on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct littleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
